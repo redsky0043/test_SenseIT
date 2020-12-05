7296ed67de2061577fb94ae5d0c74d05a7b008da
Serhii Boiko <boyko0043@gmail.com>
2020-12-05 09:31:02 +0200

    add animation

--- app/js/changeElementsOnPage.js
+++ app/js/changeElementsOnPage.js
@@ -10,12 +10,22 @@ btnApplication.addEventListener('mouseover', () => {
     sliderSecond.style.opacity = 1;
     allPixels[0].classList.add('first_pixel');
     allPixels[1].classList.add('second_pixel');
+    allPixels[2].classList.add('third_pixel');
+    allPixels[3].classList.add('fourth_pixel');
+    allPixels[4].classList.add('fifth_pixel');
+    allPixels[5].classList.add('sixth_pixel');
+    allPixels[6].classList.add('seventh_pixel');
 });
 
 btnApplication.addEventListener('mouseout', () => {
     sliderSecond.style.opacity = 0;
     allPixels[0].classList.remove('first_pixel');
     allPixels[1].classList.remove('second_pixel');
+    allPixels[2].classList.remove('third_pixel');
+    allPixels[3].classList.remove('fourth_pixel');
+    allPixels[4].classList.remove('fifth_pixel');
+    allPixels[5].classList.remove('sixth_pixel');
+    allPixels[6].classList.remove('seventh_pixel');
 });
 
 btnJury.addEventListener('mouseover', () => {
